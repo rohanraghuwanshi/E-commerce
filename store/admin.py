a57fc058416c7bb568408f9ab8f695a0944f608d
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-04-04 12:20:50 +0530

    Updated admin view

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, AdminProduct)
 admin.site.register(Category)
--- store/models.py
+++ store/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
+    category = models.ForeignKey("store.Category", default=1, on_delete=models.CASCADE)
     description = models.CharField(max_length=200, default="")
     image = models.ImageField(upload_to='Products/')
     class Meta:

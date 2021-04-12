d769f5a02f30cf0d007f6e854d47d301822069b3
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-04-12 21:05:16 +0530

    Created the user app

--- ECommerce/urls.py
+++ ECommerce/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('user/', include('user.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
